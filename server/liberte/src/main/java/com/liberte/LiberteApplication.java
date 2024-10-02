755cb87900ef52ab0b547cdc00e0988b137aeb6c
gnatykdm <gnatykwork@gmail.com>
2024-10-02 08:11:20 +0200

    app-skeleton

---
+++ server/liberte/src/main/java/com/liberte/LiberteApplication.java
@@ -0,0 +1,13 @@
+package com.liberte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiberteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiberteApplication.class, args);
+	}
+
+}
---
+++ server/liberte/src/test/java/com/liberte/LiberteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liberte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiberteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
